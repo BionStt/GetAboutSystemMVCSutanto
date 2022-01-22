37cb555df896dea935cd65007c6da0340b164435
BionStt <43228630+BionStt@users.noreply.github.com>
2022-01-22 08:42:08 +0700

    Add project files.

---
+++ Utility/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility.Enum;
+
+namespace Utility
+{
+    public class AppSettings
+    {
+        public EditorChoice Editor { get; set; }
+        public int PostAbstractWords { get; set; }
+        public IDictionary<string, int> CacheSlidingExpirationMinutes { get; set; }
+        public bool Pride { get; set; }
+    }
+}
